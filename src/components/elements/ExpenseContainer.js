97a6cec5b9ae7ff92a086c84761406016cff0c3c
Eddie Chu <contact@eddiechu.me>
2019-09-27 23:55:16 -0700

    add expense list items

--- src/components/elements/ExpenseContainer.js
+++ src/components/elements/ExpenseContainer.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import FormSelect from '../forms/FormSelect';
+import React from "react";
+import FormSelect from "../forms/FormSelect";
 
 export default class ExpenseContainer extends React.Component {
     render() {
-        return(<div className="expense-container">
-            <div className="expense-container__options">
-                <FormSelect options={["Active", "Archived"]} />
-                <button className="btn btn--alt">Add Expense</button>
+        return (
+            <div className="expense-container">
+                <div className="expense-container__options">
+                    <FormSelect options={["Active", "Archived"]} />
+                    <button className="btn btn--alt">Add Expense</button>
+                </div>
+                <div className="expense-container__content">
+                    <div className="expense-container__list-header">
+                        <p>Expense</p>
+                        <p>Type</p>
+                        <p>Amount</p>
+                        <p>Due Date</p>
+                        
+                    </div>
+                    <ul className="expense-container__list">
+                        <li className="list-item">
+                            <div className="list-item__inner">
+                                <p>Netflix</p>
+                                <p>Monthly</p>
+                                <p>$11.99</p>
+                                <p>9/20/19</p>                                
+                            </div>
+                        </li>
+                        <li className="list-item">
+                            <div className="list-item__inner">
+                                <p>Netflix</p>
+                                <p>Monthly</p>
+                                <p>$11.99</p>
+                                <p>9/20/19</p>                                
+                            </div>
+                        </li>
+                        <li className="list-item">
+                            <div className="list-item__inner">
+                                <p>Netflix</p>
+                                <p>Monthly</p>
+                                <p>$11.99</p>
+                                <p>9/20/19</p>                                
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>)
+        );
     }
 }

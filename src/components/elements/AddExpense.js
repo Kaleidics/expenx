e5860db45a728413ccbf685eac358f235c031b11
Eddie Chu <contact@eddiechu.me>
2019-10-14 00:50:12 -0700

    adding new expense form

--- src/components/elements/AddExpense.js
+++ src/components/elements/AddExpense.js
@@ -2,43 +2,93 @@ import React from "react";
 
 import FormInput from "../forms/FormInput";
 import FormSelect from "../forms/FormSelect"
+import FormTextArea from "../forms/FormTextArea"
+
+
+import FormatDate from "../../helpers/FormatDate"
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class AddExpense extends React.Component {
+
+    state = {
+        expiration: new Date()
+    }
+
+    setExpiration = date => this.setState({expiration: date });
+
     render() {
+        console.log("current date", this.state.expiration)
         return (
             <div className="addExpense-container animate-left">
-                <button className="btn btn--alt" onClick={this.props.handleContent}>
-                    Cancel
-                </button>
+                <div className="addExpense-container__options">
+                    <button className="btn btn--alt" onClick={this.props.handleContent}>
+                        Cancel
+                    </button>
+                </div>
+
                 <div className="addExpense-container__form-container">
                     <form className="addExpense-container__form">
                         <fieldset className="addExpense-container__fieldset">
-                            <FormInput
-                                id={"expense_name"}
-                                label={"Expense"}
-                                type={"text"}
-                                // error={this.state.fullnameError}
-                                // onChange={e => this.setState({ fullname: e.target.value })}
-                                // value={this.state.fullname}
-                            />
-                            <FormInput
-                                id={"expense_amount"}
-                                label={"Amount"}
-                                type={"number"}
-                                step={"0.01"}
-                                // error={this.state.fullnameError}
-                                // onChange={e => this.setState({ fullname: e.target.value })}
-                                // value={this.state.fullname}
-                            />
-                            <FormSelect
-                                id={"expense_type"}
-                                label={"Expense Type"}
-                                options={["One Time", "Daily", "Weekly", "Monthly", "Year"]}
-                                // error={this.state.typeError}
-                                // onChange={e => this.setState({ type: e.target.value })}
-                                // value={this.state.type}
-                            />
+                            <legend className="addExpense-container__legend">Add a new expense</legend>
+                            <div className="addExpense-container__form-one">
+                                <FormInput
+                                    id={"expense_name"}
+                                    label={"Expense"}
+                                    type={"text"}
+                                    // error={this.state.fullnameError}
+                                    // onChange={e => this.setState({ fullname: e.target.value })}
+                                    // value={this.state.fullname}
+                                />
+                                <FormInput
+                                    id={"expense_amount"}
+                                    label={"Amount"}
+                                    type={"number"}
+                                    step={"0.01"}
+                                    // error={this.state.fullnameError}
+                                    // onChange={e => this.setState({ fullname: e.target.value })}
+                                    // value={this.state.fullname}
+                                />
+                            </div>
+                            <div className="addExpense-container__form-two">
+                                <FormSelect
+                                    id={"expense_type"}
+                                    label={"Expense Type"}
+                                    options={["One Time", "Daily", "Weekly", "Monthly", "Year"]}
+                                    // error={this.state.typeError}
+                                    // onChange={e => this.setState({ type: e.target.value })}
+                                    // value={this.state.type}
+                                />
+                                <FormSelect
+                                    id={"expense_status"}
+                                    label={"Status"}
+                                    options={["Active", "Paid", "Archived"]}
+                                    // error={this.state.typeError}
+                                    // onChange={e => this.setState({ type: e.target.value })}
+                                    // value={this.state.type}
+                                />
+                                <div className="addExpense-datepicker">
+                                    <label className="label">Date</label>
+                                    <DatePicker selected={this.state.expiration} onChange={this.setExpiration} />
+                                </div>
+                            </div>
+                            <div className="addExpense-container__form-three">
+                                <FormTextArea
+                                    id={"expense_notes"}
+                                    label={"Notes (optional):"}
+                                    type={"text"}
+                                    rows={"5"}
+                                    cols={"10"}
+                                    // error={this.state.fullnameError}
+                                    // onChange={e => this.setState({ fullname: e.target.value })}
+                                    // value={this.state.fullname}
+                                />
+                            </div>
                         </fieldset>
+                        <div className="addExpense-container__actions">
+                            <button className="btn btn--primary">Submit</button>
+                        </div>
                     </form>
                 </div>
             </div>
